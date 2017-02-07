530642779bdc6ab2dbdd95f986623a667fbdd926
Angus Russell <angus.russell89@gmail.com>
2017-02-07 16:45:05 +1000

    test data for channel

--- src/components/channel/channel.js
+++ src/components/channel/channel.js
@@ -5,6 +5,22 @@ import Sidebar from '../sidebar/sidebar'
 import styles from './channel.css'
 
 class Channel extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      slug: 'testchannel',
+      title: 'Test Channel',
+      blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed unum dolor adispes ordinente.',
+      logo_url: '',
+      external_url: '',
+      external_facebook: '',
+      external_twitter: '',
+      external_instagram: '',
+      total_videos: 5,
+      recent_videos: '',
+      videos: ''
+    }
+  }
 
   render (props, state) {
     return (
