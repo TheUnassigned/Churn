8a8c73ffde57a6fd97bfc3fcb11cb1551e647309
Angus Russell <angus.russell89@gmail.com>
2017-02-09 12:23:45 +1000

    Moved state to app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,178 @@
 /** @jsx h */
 import { h, Component } from 'preact'
-import Channels from '../channels/channels'
+import Channel from '../channel/channel'
 import styles from './app.css'
+import findIndex from 'lodash/findIndex'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      activeVideo: 0,
+      activeChannel: 0,
+      channels: [
+        {
+          slug: 'testchannel',
+          title: 'Test Channel',
+          blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed unum dolor adispes ordinente.',
+          logo_url: 'http://newchurn.s3-website-ap-southeast-2.amazonaws.com/img/channel.png',
+          external_url: '',
+          external_facebook: '',
+          external_twitter: '',
+          external_instagram: '',
+          total_videos: 5,
+          recent_videos: [
+            {
+              channel_position: 0,
+              youtube_id: '8LSwyqBLsZ0',
+              title: '17 Types of Ah-Liens',
+              duration: '0:34'
+            },
+            {
+              channel_position: 1,
+              youtube_id: 'Dws45IJ_8Io',
+              title: 'Test',
+              duration: '0:34'
+            },
+            {
+              channel_position: 2,
+              youtube_id: 'kvji3CYA51E',
+              title: 'Test',
+              duration: '0:34'
+            },
+            {
+              channel_position: 3,
+              youtube_id: 'SGYhsHH1404',
+              title: 'Test',
+              duration: '0:34'
+            },
+            {
+              channel_position: 4,
+              youtube_id: 'vy_Ca_sCVjs',
+              title: 'Merry Christmas',
+              duration: '0:18'
+            }
+          ],
+          videos: ['8LSwyqBLsZ0', 'Dws45IJ_8Io', 'kvji3CYA51E', 'SGYhsHH1404', 'vy_Ca_sCVjs']
+        },
+        {
+          slug: 'testchannel2',
+          title: 'Another Channel',
+          blurb: 'Sed unum dolor adispes ordinente. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          logo_url: 'http://newchurn.s3-website-ap-southeast-2.amazonaws.com/img/channel.png',
+          external_url: '',
+          external_facebook: '',
+          external_twitter: '',
+          external_instagram: '',
+          total_videos: 5,
+          recent_videos: [
+            {
+              channel_position: 0,
+              youtube_id: 'Dws45IJ_8Io',
+              title: '17 Types of Ah-Liens',
+              duration: '0:34'
+            },
+            {
+              channel_position: 1,
+              youtube_id: '8LSwyqBLsZ0',
+              title: 'Test',
+              duration: '0:34'
+            },
+            {
+              channel_position: 2,
+              youtube_id: 'kvji3CYA51E',
+              title: 'Test',
+              duration: '0:34'
+            },
+            {
+              channel_position: 3,
+              youtube_id: 'SGYhsHH1404',
+              title: 'Test',
+              duration: '0:34'
+            },
+            {
+              channel_position: 4,
+              youtube_id: 'vy_Ca_sCVjs',
+              title: 'Merry Christmas',
+              duration: '0:18'
+            }
+          ],
+          videos: ['8LSwyqBLsZ0', 'Dws45IJ_8Io', 'kvji3CYA51E', 'SGYhsHH1404', 'vy_Ca_sCVjs']
+        },
+        {
+          slug: 'testchannel',
+          title: '9Gag',
+          blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed unum dolor adispes ordinente.',
+          logo_url: 'http://newchurn.s3-website-ap-southeast-2.amazonaws.com/img/channel.png',
+          external_url: '',
+          external_facebook: '',
+          external_twitter: '',
+          external_instagram: '',
+          total_videos: 5,
+          recent_videos: [
+            {
+              channel_position: 0,
+              youtube_id: 'kvji3CYA51E',
+              title: 'Channel 3',
+              duration: '0:34'
+            },
+            {
+              channel_position: 1,
+              youtube_id: 'Dws45IJ_8Io',
+              title: 'Test',
+              duration: '0:34'
+            },
+            {
+              channel_position: 2,
+              youtube_id: '8LSwyqBLsZ0',
+              title: 'Test',
+              duration: '0:34'
+            },
+            {
+              channel_position: 3,
+              youtube_id: 'SGYhsHH1404',
+              title: 'Test',
+              duration: '0:34'
+            },
+            {
+              channel_position: 4,
+              youtube_id: 'vy_Ca_sCVjs',
+              title: 'Merry Christmas',
+              duration: '0:18'
+            }
+          ],
+          videos: ['8LSwyqBLsZ0', 'Dws45IJ_8Io', 'kvji3CYA51E', 'SGYhsHH1404', 'vy_Ca_sCVjs']
+        }
+      ]
+    }
+
+    this.handleUpdateActiveVideo = this.handleUpdateActiveVideo.bind(this)
+    this.handleVideoEnded = this.handleVideoEnded.bind(this)
+  }
+
+  handleUpdateActiveVideo (newId) {
+    const newActiveVideo = findIndex(this.state.channels[this.state.activeChannel].recent_videos, { 'youtube_id': newId })
+    this.setState({
+      activeVideo: newActiveVideo
+    })
+  }
+
+  handleVideoEnded () {
+    this.setState({
+      activeVideo: this.state.activeVideo + 1
+    })
+  }
 
   render (props, state) {
     return (
       <div>
-        <Channels />
+        <Channel
+          channel={state.channels[state.activeChannel]}
+          activeVideo={state.activeVideo}
+          handleUpdateActiveVideo={this.handleUpdateActiveVideo}
+          handleVideoEnded={this.handleVideoEnded}
+          />
       </div>
     )
   }
--- src/components/channel/channel.js
+++ src/components/channel/channel.js
@@ -3,97 +3,26 @@ import { h, Component } from 'preact'
 import Player from '../player/player'
 import Sidebar from '../sidebar/sidebar'
 import styles from './channel.css'
-import findIndex from 'lodash/findIndex'
 
-class Channel extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      activeVideo: 0,
-      channelInfo: {
-        slug: 'testchannel',
-        title: 'Test Channel',
-        blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed unum dolor adispes ordinente.',
-        logo_url: 'http://newchurn.s3-website-ap-southeast-2.amazonaws.com/img/channel.png',
-        external_url: '',
-        external_facebook: '',
-        external_twitter: '',
-        external_instagram: '',
-        total_videos: 5,
-        recent_videos: [
-          {
-            channel_position: 0,
-            youtube_id: '8LSwyqBLsZ0',
-            title: '17 Types of Ah-Liens',
-            duration: '0:34'
-          },
-          {
-            channel_position: 1,
-            youtube_id: 'Dws45IJ_8Io',
-            title: 'Test',
-            duration: '0:34'
-          },
-          {
-            channel_position: 2,
-            youtube_id: 'kvji3CYA51E',
-            title: 'Test',
-            duration: '0:34'
-          },
-          {
-            channel_position: 3,
-            youtube_id: 'SGYhsHH1404',
-            title: 'Test',
-            duration: '0:34'
-          },
-          {
-            channel_position: 4,
-            youtube_id: 'vy_Ca_sCVjs',
-            title: 'Merry Christmas',
-            duration: '0:18'
-          }
-        ],
-        videos: ['8LSwyqBLsZ0','Dws45IJ_8Io','kvji3CYA51E','SGYhsHH1404','vy_Ca_sCVjs']
-      }
-    }
-
-    this.updateActiveVideo = this.updateActiveVideo.bind(this)
-    this.handleVideoEnded = this.handleVideoEnded.bind(this)
-  }
-
-  updateActiveVideo (newId) {
-    const newActiveVideo = findIndex(this.state.channelInfo.recent_videos, { 'youtube_id': newId })
-    this.setState({
-      activeVideo: newActiveVideo
-    })
-  }
-
-  handleVideoEnded () {
-    this.setState({
-      activeVideo: this.state.activeVideo + 1
-    })
-  }
-
-  render (props, state) {
-    const activeVideo = state.channelInfo.recent_videos[state.activeVideo].youtube_id
-    return (
-      <div class={styles.container}>
-        <div class={styles.player}>
-          <Player
-            activeVideo={activeVideo}
-            onVideoEnded={this.handleVideoEnded}
-            />
-        </div>
-        <div class={styles.sidebar}>
-          <Sidebar
-            channelInfo={state.channelInfo}
-            activeVideo={activeVideo}
-            onUpdateActiveVideo={this.updateActiveVideo}
-            />
-        </div>
+const Channel = props => {
+  const activeVideo = props.channel.recent_videos[props.activeVideo].youtube_id
+  return (
+    <div class={styles.container}>
+      <div class={styles.player}>
+        <Player
+          activeVideo={activeVideo}
+          onVideoEnded={props.handleVideoEnded}
+          />
       </div>
-    )
-  }
+      <div class={styles.sidebar}>
+        <Sidebar
+          channelInfo={props.channel}
+          activeVideo={activeVideo}
+          onUpdateActiveVideo={props.handleUpdateActiveVideo}
+          />
+      </div>
+    </div>
+  )
 }
 
 export default Channel
